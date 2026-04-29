75ff3e9734d193aa8c7d59f15fa2476a82c4615c
jzscsaba <johanyak.csaba@nje.hu>
2026-04-29 10:32:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Verseny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Versenyző versenyző=new Versenyző(12,"Ó Ida",new List<int> {123,345,567});
+            //Console.WriteLine(versenyző);
+            List<Versenyző> v = new List<Versenyző>(); 
+            string[] sorok=File.ReadAllLines("..\\..\\..\\verseny.txt");
+            foreach (var item in sorok)
+            {
+                string[] d = item.Split();
+                int id = int.Parse(d[0].Trim('#'));
+                string nev = d[1] + " " + d[2];
+                List<int> eredmenyek=new List<int>();
+                for (int i = 3; i < d.Length; i++)
+                {
+                    eredmenyek.Add(int.Parse(d[i]));
+                }
+                v.Add(new Versenyző(id, nev, eredmenyek) );
+            }
+            foreach (var item in v)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Verseny
+{
+    internal class Versenyző
+    {
+        private int Id;
+        private string Név;
+        public List<int> Eredmények;
+
+        public Versenyző(int id, string név, List<int> eredmények)
+        {
+            Id = id;
+            Név = név;
+            //Eredmények = new List<int>();
+            Eredmények = eredmények;
+        }
+
+        public override string ToString()
+        {
+            //id név, eredményei: eredmény1, …legjobb: legjobbEredmény
+            string s = $"{Id} {Név}, eredményei: ";
+            foreach (var item in Eredmények)
+            {
+                s += $"{item}, ";
+            }
+            s += $"legjobb: {legjobbUgrás()}";
+            return s;
+        }
+
+        private int legjobbUgrás()
+        {
+            int maxi = 0;
+            for (int i = 0; i < Eredmények.Count; i++)
+            {
+                if (Eredmények[i] > Eredmények[maxi])
+                {
+                    maxi = i;
+                }
+            }
+            return Eredmények[maxi];
+        }
+
+        public void setId(int id)
+        {
+            Id=id;
+        }
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setNév(string név)
+        {
+            Név=név;
+        }
+        public string getNév()
+        {
+            return Név;
+        }
+    }
+}
